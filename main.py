17d9538119fb22ffc8dfa1273f8b35142b3e1095
Naami-droid <mrnaami7@gmail.com>
2026-07-18 18:40:35 +0500

    feat: Initialized project with the root route

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI, HTTPException
+import pydantic
+from schema import TaskSchema
+from tasks import tasks_db
+
+app=FastAPI()
+
+@app.get("/")
+def read_root():
+    return({"message":"A TO-DO list built with FastAPI"})
+
